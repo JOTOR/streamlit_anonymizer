f93f75bb94690a3b8d7e717048b68f5f3a472b68
John Torres <jhonnatan.torres.suarez@gmail.com>
2021-09-26 18:10:16 -0500

    Add files via upload

---
+++ Anonymizer.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+
+##Main Function
+def anonymise_categorical_variable(DATAFRAME, COLS):
+    
+    for col in COLS:
+        anom = []
+        res_dict = {}
+        ind = DATAFRAME[col].unique()
+        
+        for i in range(len(ind)):
+            anom.append(col+"_"+str(i))
+        
+        for k, v in zip(ind, anom):
+            res_dict.update({k:v})        
+        
+        DATAFRAME[col+'_'+'Anonym'] = DATAFRAME[col].map(res_dict)
+    st.write("Output Dataset:")
+    st.dataframe(df.drop(columns=DIFF))
+
+st.header("The Anonymizer")
+st.markdown("Replace any sensitive **categorical** feature in a dataset, for example, if you have the countries AUS, COL, ARG these are replaced by Country_1, Country_2, Country 3")
+
+data = st.file_uploader("Upload a csv file", type=["csv"])
+
+if data is not None:
+    df = pd.read_csv(data)
+    CAT_COLS = df.select_dtypes(include=['object']).columns.tolist()
+    TO_REMOVE = st.multiselect("Columns to remove from process:", df.columns)     
+    DIFF = list(set(CAT_COLS)-set(TO_REMOVE))
+    st.write("Columns included in the process: ",str(DIFF))
+    st.write("Input Dataset:")
+    st.dataframe(df)
+
+    anonymise_categorical_variable(DATAFRAME=df, COLS=DIFF)
+    st.download_button(label="Download Data", data=df.to_csv().encode('utf-8'), file_name='df_anonym.csv')
